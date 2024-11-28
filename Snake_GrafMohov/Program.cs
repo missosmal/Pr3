26bdeddacf3601a371cdf2e5c10c5b9483b780dc
Artem <artemmohov05@mail.ru>
2024-11-28 20:14:00 +0500

    Добавлены переменные сервера. Мохов

--- Snake_GrafMohov/Program.cs
+++ Snake_GrafMohov/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Snake_GrafMohov
 {
-    internal class Program
+    public class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        public static int MaxSpeed = 15;
+
         static void Main(string[] args)
         {
+
         }
     }
 }
