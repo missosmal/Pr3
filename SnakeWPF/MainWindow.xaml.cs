0b43e021b6e82dcd7648f17cfd097adffb2f3ec1
missosmal <144768125+missosmal@users.noreply.github.com>
2024-11-29 20:09:50 +0500

    Созданные переменные для клиента игры.Graf

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Common;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SnakeWPF
 {
@@ -20,6 +11,16 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public ViewModelUserSettings ViewModelUserSettings = new ViewModelUserSettings();
+        public ViewModelGames ViewModelGames = null;
+        public static IPAddress remoteIPAddress = IPAddress.Parse("127.0.0.1");
+        public static int remotePort = 5001;
+        public Thread tRec;
+        public UdpClient receivingUdpClient;
+        public Pages.Home Home = new Pages.Home();
+        public Pages.Game Game = new Pages.Game();
+
         public MainWindow()
         {
             InitializeComponent();
