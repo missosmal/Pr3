1a061898081819dfe8b2777454ec99ce0d9e88ae
missosmal <144768125+missosmal@users.noreply.github.com>
2024-11-29 20:11:26 +0500

    Метод запускающий прослушивание ответа от сервера.Graf

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,9 +1,24 @@
 ﻿using Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
-
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 namespace SnakeWPF
 {
     /// <summary>
@@ -24,6 +39,12 @@ namespace SnakeWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void StartReceiver()
+        {
+            tRec = new Thread(new ThreadStart(Receiver));
+            tRec.Start();
         }
     }
 }
